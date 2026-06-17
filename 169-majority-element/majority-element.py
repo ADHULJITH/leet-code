d6790f72c12db0c00c809e00560e4bd87105a5e1
ADHULJITH <149286698+ADHULJITH@users.noreply.github.com>
2026-06-17 13:26:14 +0530

    Time: 14 ms (30.52%) | Memory: 13.6 MB (40.06%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        d={}
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1    
+        for k,v in d.items():
+            if v>len(nums)//2:
+                return k      
